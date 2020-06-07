74e283bafdc5a9c917e7d968bec2820a848ec768
Donat Pllana <donat.pllana@outlook.com>
2020-06-07 04:54:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,6 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   bash_hash={:railroads=>{}}
+  bash_hash
 
 end
 
@@ -10,6 +11,12 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  bash_hash={:railroads=>{
+    :pieces=>4,
+    :rent_in_dollars=>{},
+    :names=>{}
+    }}
+
 
 end
 
